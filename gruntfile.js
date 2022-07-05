604a0b57a2cb943737cefbf932e6cebaaf82bd7a
马国昌 <yolinsoft@163.com>
2022-07-05 14:01:18 +0800

    修改 nwjs版本

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,17 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-      nwjs: {
-        options: {
-            version: '0.12.1',
-            macIcns: './assets/osx/icon.icns',
-            winIco: './assets/win/icon.ico',
-            platforms: ['win'],
-            buildDir: './build', // Where the build version of my NW.js app is saved
-        },
-        src: ['./panda/**/**'] // Your NW.js app
+module.exports = function (grunt) {
+  grunt.initConfig({
+    nwjs: {
+      options: {
+        version: '0.14.7',
+        macIcns: './assets/osx/icon.icns',
+        winIco: './assets/win/icon.ico',
+        platforms: ['osx64'],
+        buildDir: './build' // Where the build version of my NW.js app is saved
       },
-    });
+      src: ['./panda/**/**'] // Your NW.js app
+    }
+  })
 
-    grunt.loadNpmTasks('grunt-nw-builder');
-    grunt.registerTask('default', ['nwjs']);
-
+  grunt.loadNpmTasks('grunt-nw-builder')
+  grunt.registerTask('default', ['nwjs'])
 }
-
