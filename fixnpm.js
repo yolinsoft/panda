3828635935e433b4f710aa717793dc97f2bb61b4
马国昌 <yolinsoft@163.com>
2022-07-05 14:00:41 +0800

    修复 证书问题

---
+++ fixnpm.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+const files = [
+  // 修复证书失效的问题
+  {
+    path: 'nw-builder/lib/versions.js',
+    replace: {
+      8: "process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';"
+    }
+  }
+]
+
+files.forEach(file => {
+  const path = `./node_modules/${file.path}`
+  if (!fs.existsSync(path)) {
+    return
+  }
+  console.log(path)
+  const content = fs.readFileSync(path).toString().split('\n')
+  const lines = Object.keys(file.replace)
+  lines.forEach(line => {
+    content[line - 1] = file.replace[line]
+  })
+  fs.writeFileSync(path, content.join('\n'))
+})
+
+console.log('组件修复完成')
